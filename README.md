8f921893aa05594fc87d7392ea1b5bb4a8641fb0
Masato Naka <masatonaka1989@gmail.com>
2022-10-06 22:46:18 +0900

    Enable to compare with estimated death

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ pip install -r requirements.txt
 python visualize.py
 ```
 
-![](docs/image-01.png)
+![](docs/image-02.png)
 
 ## Data
 
@@ -21,6 +21,9 @@ python visualize.py
         ```
         cat data/exdeath-japan-observed.csv| cut -f1,2,3 -d, | sort -n| uniq > data/prefecture_master.csv
         ```
+1. [日本の超過及び過小死亡数ダッシュボード (死因別死亡数)](https://exdeaths-japan.org/graph/weekly_cause): data/exdeath-japan-cause
+1. [人口動態調査](https://www.e-stat.go.jp/stat-search/files?page=1&toukei=00450011&tstat=000001028897&cycle=1&year=20220&tclass1=000001053058&tclass2=000001053060&cycle_facet=tclass1%3Atclass2%3Acycle&tclass3val=0)
+1. [人口動態統計速報(令和４年７月分)](https://www.mhlw.go.jp/toukei/saikin/hw/jinkou/geppo/s2022/dl/202207.pdf)
 
 ## Reference
 1. https://plotly.com/python/figure-structure/
--- visualize.py
+++ visualize.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from functools import lru_cache
 from time import perf_counter
 from tkinter import ALL
+from turtle import width
 
 import ndjson
 import pandas as pd
@@ -14,27 +15,38 @@ import plotly.graph_objects as go
 from dash import Dash, Input, Output, dcc, html
 from plotly.subplots import make_subplots
 
-prefecture_master = pd.read_csv('data/prefecture_master.csv').loc[:, ('prefecture_id', 'prefectureJP')].rename(
-    columns={'prefecture_id': 'value', 'prefectureJP': 'label'}).T.to_dict().values()
+pd.options.plotting.backend = "plotly"
 
+
+prefecture_master_df = pd.read_csv('data/prefecture_master.csv')
+prefecture_master = prefecture_master_df.loc[:, ('prefecture_id', 'prefectureJP')].rename(
+    columns={'prefecture_id': 'value', 'prefectureJP': 'label'}).T.to_dict().values()
+prefecture_id2name = prefecture_master_df.loc[:,
+                                              ('prefecture_id', 'prefecture_EN')].set_index('prefecture_id').to_dict()['prefecture_EN']
 ALL_JAPAN_ID = 48
-START_DATE = "2020-01-01"
+START_DATE = "2017-01-01"
 
 
 def run():
 
     df = load_data()
-    df = df[df['prefecture_id'] == ALL_JAPAN_ID]
 
-    fig = make_fig(df, "")
+    fig = make_fig(df, ALL_JAPAN_ID)
 
     app = Dash(__name__)
     app.layout = html.Div([
-        html.H4('Covid-19 Analysis'),
+        html.H2('Covid-19 Analysis'),
+        html.H4('Prefecture'),
         dcc.Dropdown(
             options=list(prefecture_master),
             value=ALL_JAPAN_ID,
             id='demo-dropdown'),
+        html.H4('Death Type'),
+        dcc.Dropdown(
+            options=[{"value": 1, "label": "Death Exceeded"},
+                     {"value": 2, "label": "Death"}],
+            value=2,
+            id='death-dropdown'),
         html.Div(id='dd-output-container'),
         dcc.Graph(id="graph", figure=fig),
         dcc.Clipboard(target_id="structure"),
@@ -57,8 +69,9 @@ def run():
             return
         print(f"update_graph {value}")
         df = load_data()
-        df = df[df['prefecture_id'] == int(value)]
-        fig = make_fig(df, f"Vaccination & Death")
+        prefecture_id = int(value)
+
+        fig = make_fig(df, prefecture_id)
         fig.update_layout(transition_duration=0)
         return fig
 
@@ -91,6 +104,14 @@ def read_death():
     return df.loc[:, ('week_ending_date', 'Observed', 'prefecture_id')].rename(columns={'week_ending_date': 'date', 'Observed': 'death'})
 
 
+def read_estimated_death():
+    df = pd.read_csv('data/exdeath-japan-estimates.csv')
+    df['week_ending_date'] = pd.to_datetime(
+        df['week_ending_date'], format="%d%b%Y")  # 17feb2013
+    df = df[(df['week_ending_date'] > START_DATE)]
+    return df.rename(columns={'week_ending_date': 'date'})
+
+
 def get_prefecture_id(prefecture_name):
     '''Get prefecture id from data/exdeath-japan-observed.csv.
     ToDo: use the prefecture list for master data.
@@ -103,7 +124,7 @@ def get_prefecture_id(prefecture_name):
 def load_data():
     print("loading data")
     t = time.time()
-    df_death = read_death()
+    df_death = read_estimated_death()
     print(f"df_death {df_death.shape}")
     df_vaccination = read_vaccination()
     print(f"df_vaccination {df_vaccination.shape}")
@@ -117,14 +138,27 @@ def load_data():
     return df
 
 
-def make_fig(df, title):
+def make_fig(df, prefecture_id):
+    prefecture_name = prefecture_id2name[prefecture_id]
+    df = df[(df['prefecture_id'] == prefecture_id)
+            | (df['prefecture_id'].isnull())]
+    df[f"{prefecture_name}_Exceeded"] = df[f"{prefecture_name}_Observed"] - \
+        df[f"{prefecture_name}_Estimated"]
+
     fig = make_subplots(specs=[[{"secondary_y": True}]])
-    fig.add_trace(
-        go.Scatter(
-            x=df['date'],
-            y=df['death'],
-            name='death'),
-        secondary_y=False)
+    death_type = 1
+    if death_type == 1:
+        death_data = [f"{prefecture_name}_95Lower", f"{prefecture_name}_95Upper",
+                      f"{prefecture_name}_Observed", f"{prefecture_name}_Estimated"]
+    elif death_type == 2:
+        death_data = [f"{prefecture_name}_Exceeded"]
+    for y in death_data:
+        fig.add_trace(
+            go.Scatter(
+                x=df['date'],
+                y=df[y],
+                name=f"death_{y}"),
+            secondary_y=False)
 
     fig.add_trace(
         go.Scatter(
@@ -133,7 +167,23 @@ def make_fig(df, title):
             name="vaccination"),
         secondary_y=True)
     fig.update_layout(
-        title_text=title)
+        title_text=f"Vaccination & Death ({prefecture_name})")
+
+    return fig
+
+
+def make_fig_from_estimates(df, title='test'):
+    prefecture = "Japan"
+    df[f"{prefecture}_Exceeded"] = df[f"{prefecture}_Observed"] - \
+        df[f"{prefecture}_Estimated"]
+
+    fig = px.line(
+        df,
+        x="date",
+        y=["Japan_95Lower", "Japan_95Upper", "Japan_Observed", "Japan_Estimated"],
+        title="Wide-Form Input, relabelled",
+        labels={"value": "death", "week_ending_date": "date"}
+    )
     return fig
 
 
